72d9aee3798bffefecb6377d7b86bcfd14643846
Nand-Kishore2000 <nanduchhimpa414243@gmail.com>
2026-03-07 15:49:31 +0530

    Learned two sum pair problem by myself without looking solution or hint

---
+++ Array/twosumPair.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Question: we have given arr unique integer array and a
+//  target and we need to return the pair of two numbers which sum would be equals to target
+// e.g [1,7,3,5,8,21,23,0] , target = 8 so output: {{1,7},{3,5}{8,0}}
+//THEORY: in the brute force approach we were using another for loop to look into the remaining array for the complement value
+// which (currentvalue - target) so instead a loop we can use a hashmap where we will insertthe iterated values 
+// and when we have to look for the 2nd value we can check in O(1) and insert that value into ans vector array.
+
+//OPTIMAL APPROACH O(N2)
+vector<pair<int, int>> twoPairSum(vector<int> nums, int target)
+{
+    vector<pair<int, int>> ans;
+    unordered_map<int,int> mpp;
+
+    for(int i=0; i<nums.size(); i++){
+        int complement = target - nums[i];
+        if(mpp.count(complement)){
+            ans.push_back({complement, nums[i]});
+        }
+        mpp[nums[i]] = i;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr = {1, 7, 3, 5, 8, 21, 23, 0};
+    int target = 8;
+
+    vector<pair<int, int>> result  = twoPairSum(arr,target);
+
+    for( auto i: result){
+        cout << "{" << i.first <<"," << i.second << "}";
+    }
+    
+     return 0;
+}
+
